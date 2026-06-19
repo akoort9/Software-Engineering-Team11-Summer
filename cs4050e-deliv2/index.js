27fdc71a89d8b0b8d3a538be3130e8b0e9ef7c4f
Juniper Shepherd <jas01620@uga.edu>
2026-06-18 22:20:41 -0400

    typo fixed

--- cs4050e-deliv2/index.js
+++ cs4050e-deliv2/index.js
@@ -1,6 +1,8 @@
 // Runs a simple server on http://localhost:8000
 const express = require('express');
 const app = express();
-app.listen(8000, () => console.log('listening at port 3000'));
+
+// host 'public' and listen
+app.listen(8000, () => console.log('listening at port 8000'));
 app.use(express.static('public'));
 
