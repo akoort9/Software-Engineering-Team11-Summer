886216c4ed4c0948d822a6a9603bc6dceec876be
Juniper Shepherd <jas01620@uga.edu>
2026-06-22 11:02:21 -0400

    Merged Listing and Movie classes

--- backend/src/main/java/cs4050e/deliv2/db/Movie.java
+++ backend/src/main/java/cs4050e/deliv2/db/Movie.java
@@ -17,19 +17,47 @@ public class Movie {
     /** Placeholder. Represents URI to movie trailer. */
     String trailer;
 
+    /** On a 0 to 10 scale, 5 representing 2.5 stars, etc. */
+    int rating;
+
+    /**
+     * {@code true} - "Currently Running"
+     * {@code false} - "Coming Soon"
+     */
+    boolean status;
+
+    /** Hardcoded, here to make it easier to change later. */
+    private String[] showtimes = {"2:00 PM", "5:00 PM", "8:00 PM"};
+    
     /**
      * Initializes a {@code Movie} object.
      * @param title The movie's title.
      * @param desc The movie's description.
      * @param poster The movie's promotional poster.
      * @param trailer The movie's promotional trailer.
+     * @param rating The movie's rating.
+     * @param status Whether the movie is 'Currently Showing' or not ('Coming Soon').
+     * @throws IllegalArgumentException if {@code rating} is not between 0 and 10.
      */
-    public Movie(String title, String genre, String desc, String poster, String trailer) {
+    public Movie(String title,
+		 String genre,
+		 String desc,
+		 String poster,
+		 String trailer,
+		 int rating,
+		 boolean status) {
+	
 	this.title = title;
 	this.genre = genre;
 	this.desc = desc;
 	this.poster = poster;
 	this.trailer = trailer;
+	this.status = status;
+	if (rating < 0 || rating > 10) {
+	    throw new IllegalArgumentException("rating must be between 0 and 10");
+	} else {
+	    this.rating = rating;
+	} // if-else
     } // Movie
 
     /**
@@ -48,8 +76,16 @@ public class Movie {
 	boolean sameDesc = this.desc.equals(movie.desc);
 	boolean samePoster = this.poster.equals(movie.poster);
 	boolean sameTrailer = this.trailer.equals(movie.trailer);
+	boolean sameRating = this.rating == movie.rating;
+	boolean sameStatus = this.status == movie.status;
 
-	return sameTitle && sameGenre && sameDesc && samePoster && sameTrailer;
+	return (sameTitle &&
+		sameGenre &&
+		sameDesc &&
+		samePoster &&
+		sameTrailer &&
+		sameRating &&
+		sameStatus);
     } // compare
     
 } // Movie
